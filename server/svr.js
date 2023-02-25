3eab7e7bf8ed558ec03eea7c8d95faf808d7e487
manighahrmani <mashapicasso@gmail.com>
2023-02-25 22:13:41 +0000

    Merge messageboard and svr

--- server/svr.js
+++ server/svr.js
@@ -1,17 +1,34 @@
-import * as mb from './messageboard.js';
-
 import express from 'express';
+import uuid from 'uuid-random';
 
 const app = express();
-
+// serve static files from the client directory with the .html extension
 app.use(express.static('../client', { extensions: ['html'] }));
 
-function getMessages(req, res) {
-  res.json(mb.listMessages());
+let messages = [
+  { id: 'xnshfdsafasd', msg: 'these are three default messages', time: 'an hour ago' },
+  { id: 'dskjdshkjhsd', msg: 'delivered from the server', time: 'yesterday' },
+  { id: 'vcxbxcvfggzv', msg: 'using a custom route', time: 'last week' },
+];
+
+/* Find a message by id */
+function findMessage(id) {
+  for (const message of messages) {
+    if (message.id === id) {
+      return message;
+    }
+  }
+  return null;
 }
 
+/* Respond to the request with all messages */
+function getMessages(req, res) {
+  res.json(messages);
+}
+
+/* Respond to the request (that contains the id of a message) with a message matching that id */
 function getMessage(req, res) {
-  const result = mb.findMessage(req.params.id);
+  const result = findMessage(req.params.id);
   if (result) {
     res.json(result);
   } else {
@@ -19,14 +36,36 @@ function getMessage(req, res) {
   }
 }
 
+/**
+ * Given a request that contains the msg for a new message,
+ * create a new message and add it to the list of messages
+ */
 function postMessage(req, res) {
-  const messages = mb.addMessage(req.body.msg);
+  const newMessage = {
+    id: uuid(),
+    time: Date(),
+    msg: req.body.msg,
+  };
+  // messages = [newMessage, ...messages]; // add new message to front of array
+  // messages = messages.slice(0, 9); // keep only the first 10 messages
+  messages = [newMessage, ...messages.slice(0, 9)];
   res.json(messages);
 }
 
+/**
+ * Given a request that contains a message object with an id,
+ * find the message with that id and update it with the new message
+ */
 function putMessage(req, res) {
-  const message = mb.editMessage(req.body);
-  res.json(message);
+  const updatedMessage = req.body;
+  const storedMessage = findMessage(updatedMessage.id);
+  if (storedMessage == null) throw new Error('message not found');
+
+  // update old message in place
+  storedMessage.time = Date();
+  storedMessage.msg = updatedMessage.msg;
+
+  res.json(storedMessage);
 }
 
 app.get('/messages', getMessages);
