9ef6f2e2d8e208d9bc7e286a367c644ae5bf0b40
manighahrmani <mashapicasso@gmail.com>
2023-02-25 12:06:19 +0000

    Move server files to their own folder

--- server/svr.js
+++ server/svr.js
@@ -6,7 +6,7 @@ import express from 'express';
 
 const app = express();
 
-app.use(express.static('client', { extensions: ['html'] }));
+app.use(express.static('../client', { extensions: ['html'] }));
 
 function getMessages(req, res) {
   res.json(mb.listMessages());
